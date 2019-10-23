19e2f5e5ab613546fc8c71101de17f6a0df0f26a
jian玄冰 <jianxuanhuo1@126.com>
2019-10-24 00:52:52 +0800

    feat: 新增控制台输出

--- src/Bing.Logging/Bing/Logging/ILogger.cs
+++ src/Bing.Logging/Bing/Logging/ILogger.cs
@@ -1,10 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Bing.Logging
 {
-    interface ILogger
+    /// <summary>
+    /// 日志操作
+    /// </summary>
+    public interface ILogger
     {
+        /// <summary>
+        /// 检查给定日志级别是否启用
+        /// </summary>
+        /// <param name="level">日志级别</param>
+        bool IsEnabled(LogLevel level);
+
+        /// <summary>
+        /// 写入日志
+        /// </summary>
+        /// <param name="level">日志级别</param>
+        /// <param name="message">日志消息</param>
+        /// <param name="exception">异常信息</param>
+        void Log(LogLevel level, string message, Exception exception);
     }
 }
--- src/Bing.Logging/Bing/Logging/ILoggerProvider.cs
+++ src/Bing.Logging/Bing/Logging/ILoggerProvider.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Bing.Logging
 {
-    interface ILoggerProvider
+    /// <summary>
+    /// 日志提供程序
+    /// </summary>
+    public interface ILoggerProvider : IDisposable
     {
+        /// <summary>
+        /// 创建一个新的<see cref="ILogger"/>实例
+        /// </summary>
+        /// <param name="name">日志名称</param>
+        ILogger CreateLogger(string name);
     }
 }
